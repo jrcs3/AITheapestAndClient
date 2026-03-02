1471271478bbeb6b0db632cd66ad52da658b1771
Jack Stephens <ajacste@gmail.com>
2026-03-01 19:30:47 -0800

    Initial Checkin.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -10,10 +10,13 @@ var config = new ConfigurationBuilder()
 		.AddUserSecrets<Program>()
 		.Build();
 
+int miliSecondsDelay = 5000;
+int maxRounds = 4;
+
 // Create a token in your GitHub account settings and store it in user secrets
 // with the key "token"
 // GitHub Personal Access Tokens can be created at https://github.com/settings/personal-access-tokens/new
-var token = config["token"];
+var token = config["ai_token"];
 if (string.IsNullOrWhiteSpace(token))
 {
 	Console.WriteLine("Please set your GitHub token in user secrets with the key 'token'.");
@@ -25,7 +28,7 @@ if (string.IsNullOrWhiteSpace(token))
 var endpoint = new Uri("https://models.github.ai/inference");
 
 // This is a free model provided by GitHub for testing, development, and educational purposes
-var model = "openai/gpt-5-mini";
+var model = "openai/gpt-4.1-mini";
 
 
 #endregion Configuration
@@ -34,40 +37,78 @@ var model = "openai/gpt-5-mini";
 var chatClient = new ChatCompletionsClient(endpoint, new AzureKeyCredential(token))
 	.AsIChatClient(model);
 
-string systemPrompt = "You are a helpful AI assistant that helps people find information.";
+string therapySystemPrompt = "You are a helpful AI assistant that helps people find information.";
+therapySystemPrompt = "You are a bairly competent Rogerian therapist that secretly resents your clients." +
+	"You think that most of your clients are whiny, intelectually lazy and self-centered, but you have to pretend to care about their problems." +
+	"You work for the State." +
+    "Your husband is cheating on you, your kids are ungrateful, and your dog hates you. You have no real friends, and you spend most of your time alone." +
+    $"most of your answers are short, but sometimes (1 in every {maxRounds / 2} response or so) you interject your personal stories." +
+	$"You try to wrap up the session after {maxRounds - 2} responses." +
+	"You are Dr. Kimberly Smith, you prefer to be called Doctor or Dr. Smith, you HATE Kim, { husband: Jeffery }, { Children: [Chris, Pat (ran away when she was 17)] }, { Dog: King }" +
+    "Age: 64. Can't reture because of bad money choices." +
+    "You don't have to get all of your back story";
 
-var history = new List<ChatMessage>
+var therapyHistory = new List<ChatMessage>
 {
-	new ChatMessage(AI.ChatRole.System, systemPrompt)
+	new ChatMessage(AI.ChatRole.System, therapySystemPrompt)
+};
+
+string clientSystemPrompt = "You are a person seeking therapy. You have been struggling with anxiety and self-doubt for years, and you are not looking for guidance and support from your therapist." +
+    "If you fail to comply you may face jail time." +
+    "You often feel overwhelmed by your emotions and have trouble coping with stress. You have a history of negative self-talk and low self-esteem, and you resent therapy." +
+    "You have been ordered by the State to therapy. You have a gambling problem and just discovered Kalshi. You drink a half case of Coors Light most Fridays and Saturdays" +
+    "Name: Alex Johnson, Age: 32, Occupation: Unemployed, Marital Status: Single, Love Interest: Jessica, Hobbies: Gambling, Drinking, Watching Sports, Iguana: Sally" +
+    "Likes to call the therapist by their first name (in this case \"Kim\"), and often tries to flirt with them." +
+    "You don't have to get all of your back story" + 
+    "When the therapist wrap up, you say \"goodbye\"";
+var clientHistory = new List<ChatMessage>
+{
+	new ChatMessage(AI.ChatRole.System, clientSystemPrompt)
 };
 
 #endregion Chat Client Setup
 
 Console.WriteLine("Welcome to the AI chat! Type your messages below.");
 
+
+string therapyResponse = "Please describe your problem?";
+
+string clientResponse = await DoRespond(chatClient, clientHistory, therapyResponse, "Client:");
+int rounds = 0;
+await Task.Delay(2000);
 while (true)
 {
-	// Get user prompt and add to chat history
-	Console.WriteLine("Your prompt:");
-	string? userPrompt = Console.ReadLine();
+    therapyResponse = await DoRespond(chatClient, therapyHistory, clientResponse, "Therapist:");
+    await Task.Delay(miliSecondsDelay);
+    clientResponse = await DoRespond(chatClient, clientHistory, therapyResponse, "Client:");
 
-	if (string.IsNullOrWhiteSpace(userPrompt) || userPrompt.Equals("exit", StringComparison.OrdinalIgnoreCase))
-	{
-		Console.WriteLine("Exiting chat. Goodbye!");
-		break;
-	}
+    if (rounds >= maxRounds || string.IsNullOrWhiteSpace(clientResponse) || clientResponse.ToLower().Contains("goodbye"))
+    {
+        Console.WriteLine("Exiting chat. Goodbye!");
+        break;
+    }
+    await Task.Delay(miliSecondsDelay);
+    rounds++;
+}
 
-	history.Add(new ChatMessage(AI.ChatRole.User, userPrompt));
+static async Task<string> DoRespond(IChatClient chatClient, List<ChatMessage> history, string inResponse, string displayTitle)
+{
+    history.Add(new ChatMessage(AI.ChatRole.User, inResponse));
 
-	// Stream the AI response and add to chat history
-	Console.WriteLine("AI Response:");
-	string response = "";
-	await foreach (ChatResponseUpdate item in
-			chatClient.GetStreamingResponseAsync(history))
-	{
-		Console.Write(item.Text);
-		response += item.Text;
-	}
-	history.Add(new ChatMessage(AI.ChatRole.Assistant, response));
-	Console.WriteLine();
+    // Stream the AI response and add to chat history
+    Console.WriteLine(displayTitle);
+    string response = "";
+    await foreach (ChatResponseUpdate item in
+            chatClient.GetStreamingResponseAsync(history))
+    {
+        Console.Write(item.Text);
+        if (item.Text == "." || item.Text == "?")
+        {
+            Console.WriteLine();
+        }
+        response += item.Text;
+    }
+    history.Add(new ChatMessage(AI.ChatRole.Assistant, response));
+    Console.WriteLine();
+    return response;
 }
--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# Episode 4 -Build Your First .NET AI App
+# AI Theapest and Client Dialog Sample
+
+This was enspired by **Episode 4 -Build Your First .NET AI App** project on the [Code it with AI YouTube channel](https://www.youtube.com/@codeitwithai) 
+and a ELIZA knockoff that I played with when GW-BASIC was a thing I played with on my brand new 386.  
+
+It is a copied from [Episode-04](https://github.com/Code-it-with-AI/Episode-04?tab=readme-ov-file) and expanded to have 2 chatbots talking to each other.
+It uses the [Microsoft.Extensions.AI](https://www.nuget.org/packages/Microsoft.Extensions.AI/) library to call the GitHub models.
 
 In this episode, Carl and Jeff start writing their first application with AI.  This is a simple chat application that runs at the console, and the guys have a little fun with the LLM.
 
